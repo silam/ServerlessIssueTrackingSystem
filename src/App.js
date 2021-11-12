6530efb04fe3ed76ab010a6eca7008348330e752
Si Lam <silam@hotmail.com>
2021-11-12 17:17:52 -0600

    Initial checkin for Serverless Issue Tracking System

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img  className="App-logo" />
+        <p>
+            <h1>Serverless Issue Tracking System</h1>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
